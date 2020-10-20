948548e5a892931e498b7f7284ba60e0e8506d84
Ralf Prezia <62730168+ralfprezia@users.noreply.github.com>
2020-10-19 22:33:59 -0300

    Add files via upload

---
+++ conversor_dollar/conversor_dollar/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace conversor_dollar {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("");
+
+            Console.WriteLine("Qual a cotação do dólar hoje?");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Quantos dólares pretende comprar?");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double result = ConversorDeMoeda.DolarParaReal(quantia, cotacao);
+
+            Console.WriteLine("Valor pago em reais = " + result.ToString("F2", CultureInfo.InvariantCulture));
+
+
+            
+        }
+    }
+}
